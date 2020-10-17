4b723f9253467f7df704b9d61912f80be713e1e5
kai-blt <blaitong@gmail.com>
2020-10-16 21:36:17 -0700

    Completed basic Instructor signup form

--- src/components/InstructorForm.js
+++ src/components/InstructorForm.js
@@ -1,8 +1,228 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
+import * as yup from 'yup'
+import instructor_schema from '../schemas/instructor_schema'
+
+
+
+//Styled Components
+const ErrorContainer = styled.div`
+    color: red;
+`;
+
+//Default Form Values
+const defaultFormValues = {
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
+    crossfit: false,
+    hiit: false,
+    cardio: false,
+    bodybuilding: false,
+    yoga: false,
+    pilates: false,
+    monwedfri: false,
+    tuethurs: false,
+    satsun: false,
+    about_me: ''
+}
+
+//Default Error State
+const defaultErrors = {
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
+    crossfit: false,
+    hiit: false,
+    cardio: false,
+    bodybuilding: false,
+    yoga: false,
+    pilates: false,
+    monwedfri: false,
+    tuethurs: false,
+    satsun: false,
+    about_me: ''
+}
+
+
 
 export default function InstructorForm(props) {
+
+    //Keep track of form values
+    const [formValues, setFormValues] = useState(defaultFormValues);
+
+    //Keep track of errors
+    const [errors, setErrors] = useState(defaultErrors)
+
+    //Keep button state
+    const [disabled, setDisabled] = useState(true)
+
+    //Instantiate useHistory hook
+    const history = useHistory();
+
+    //Effect Hook: Check if form valid on user input, if so enable submit
+    useEffect(() => {
+        instructor_schema.isValid(formValues)
+            .then(valid => setDisabled(!valid));
+    }, [formValues])
+
+    //Handlers 
+    const handleChange = (event) => {
+        //destructure event.target
+        const { name, value, type, checked } = event.target;
+
+        //Determine if there's a checkbox if so use checked value instead
+        const valueToUse = type === 'checkbox' ? checked : value;
+
+        //Each time user interacts reach into Yup schema
+        yup.reach(instructor_schema, name)
+            .validate(valueToUse)
+            //If valid, clear error log else add errors for display
+            .then(valid => setErrors({ ...errors, [name]: '' }))
+            .catch(error => setErrors({ ...errors, [name]: error.errors[0] }))
+        
+
+        setFormValues({ ...formValues, [name]: valueToUse });
+    };
+
+    const submit = (event) => {
+        //Prevent default form behaviour
+        event.preventDefault();
+        
+    };
+
+
+
     return(
-        <h1>Instructor Form</h1>
+        <>
+        <h2>Instructor Form</h2>
+        <ErrorContainer>
+            <div>{errors.name}</div>
+            <div>{errors.email}</div>
+            <div>{errors.address}</div>
+            <div>{errors.phone}</div>
+        </ErrorContainer>
+        <form onSubmit={submit}>
+            <h3>Info</h3>
+                <label>Name&nbsp;
+                    <input
+                        type="text"
+                        name="name"
+                        value={formValues.name}
+                        onChange={handleChange}
+                    />
+                </label><br/>
+                <label>Email&nbsp;
+                    <input
+                        type="email"
+                        name="email"
+                        value={formValues.email}
+                        onChange={handleChange}
+                    />
+                </label><br/>
+                <label>Address&nbsp;
+                    <input
+                        type="text"
+                        name="address"
+                        value={formValues.address}
+                        onChange={handleChange}
+                    />
+                </label><br/>
+                <label>Phone&nbsp;
+                    <input
+                        type="tel"
+                        name="phone"
+                        value={formValues.phone}
+                        onChange={handleChange}
+                    />
+                </label><br/>
+            <h3>Instructional Expertise</h3>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="crossfit"
+                        value={formValues.crossfit}
+                        onChange={handleChange}
+                    />Crossfit
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="hiit"
+                        value={formValues.hiit}
+                        onChange={handleChange}
+                    />H.I.I.T
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="cardio"
+                        value={formValues.cardio}
+                        onChange={handleChange}
+                    />Cardio
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="bodybuilding"
+                        value={formValues.bodybuilding}
+                        onChange={handleChange}
+                    />Bodybuilding
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="yoga"
+                        value={formValues.yoga}
+                        onChange={handleChange}
+                    />Yoga
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="pilates"
+                        value={formValues.pilates}
+                        onChange={handleChange}
+                    />Pilates
+                </label>
+            <h3>Availability</h3>
+            <label>
+                    <input
+                        type="checkbox"
+                        name="monwedfri"
+                        value={formValues.monwedfri}
+                        onChange={handleChange}
+                    />Monday | Wednesday | Friday
+                </label><br/>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="tuethurs"
+                        value={formValues.tuethurs}
+                        onChange={handleChange}
+                    />Tuesday | Thursday
+                </label><br/>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="satsun"
+                        value={formValues.satsun}
+                        onChange={handleChange}
+                    />Saturday | Sunday
+                </label>
+            <h3>About Me</h3>
+                <textarea
+                    type="text"
+                    name="about_me"
+                    value={formValues.about_me}
+                    onChange={handleChange}
+                />
+            <br/>
+            <button disabled={true}>Submit</button>
+        </form>
+        </>
     )
 }
---
+++ src/schemas/instructor_schema.js
@@ -0,0 +1,40 @@
+import * as yup from 'yup'
+
+const instructor_schema = yup.object().shape({
+    name: yup
+        .string()
+        .required('Please enter your name')
+        .min(4, 'Please use a name greater than 4 characters'),
+    email: yup
+        .string()
+        .required('Please enter your email'),
+    address: yup
+        .string()
+        .required('Please enter your address')
+        .min(5, 'Please enter a valid address'),
+    phone: yup
+        .string()
+        .required('Please enter your phone number'),
+    crossfit: yup
+        .boolean(),
+    hiit: yup
+        .boolean(),
+    cardio: yup
+        .boolean(),
+    bodybuilding: yup
+        .boolean(),
+    yoga: yup
+        .boolean(),
+    pilates: yup
+        .boolean(),
+    monwedfri: yup
+        .boolean(),
+    tuethurs: yup
+        .boolean(),
+    satsun: yup
+        .boolean(),
+    about_me: yup
+        .boolean()
+});
+
+export default instructor_schema
