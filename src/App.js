c9b316858da01959dcd42bec19fd61910379d44e
kai-blt <blaitong@gmail.com>
2020-10-17 11:10:03 -0700

    basic homepage styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,25 @@ import InstructorSignUp from './components/InstructorSignUp'
 
 
 const AppContainer = styled.div`
-    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+
+    h1 {
+        font-size: 5rem;
+        margin: 2%;
+    }
+
+    nav {
+        padding: 2%;
+        width: 100%;
+    }
+
+    a {
+        margin: 2%;
+    }
 `;
 
 
