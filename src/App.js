e99cd73a1dc5a8e1faee194b395778a8e0374f31
kai-blt <blaitong@gmail.com>
2020-10-16 17:36:07 -0700

    basic site framework non-styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
- 
+import { NavLink, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import Homepage from './components/Homepage'
+import ClientForm from './components/ClientForm'
+import InstructorForm from './components/InstructorForm'
+
+
+const AppContainer = styled.div`
+    width: 100%;
+`;
+
+
 export default function App(props) {
     return(
-        <h1>Project Ready</h1>
+        <AppContainer>
+            <nav>                
+                <h1>Anywhere Fitness</h1>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/clients'>Clients</NavLink>
+                <NavLink to='/instructors'>Instructors</NavLink>
+            </nav>
+            <Switch>
+                <Route path='/instructors'>
+                    <InstructorForm />
+                </Route>
+                <Route path='/clients'>
+                    <ClientForm />
+                </Route>
+                <Route path='/'>
+                    <Homepage />
+                </Route>
+            </Switch>
+        </AppContainer>
     )
 }
