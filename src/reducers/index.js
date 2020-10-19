a9ee7ad71cd3c42ae8a8b1cd2dbb41a2a885124f
Suzanne Cabral <suzannecabral@live.com>
2020-10-19 08:19:24 -0700

    Added actions file and reducer, need to connect and test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { TEST_ACTION } from '../actions';
 
 const initialState={
-    testKey:true
+    testKey:""
 };
 export const reducer = (state = initialState, action) => {
+    console.log("reducer received action: ", action);
     switch(action.type){
+
+        case TEST_ACTION:
+                const testState = {
+                    ...state,
+                    testKey:action.payload
+                }
+                console.log("reducer: test action completed");
+                return testState;
         default:
+                console.log("reducer defaulted");
                 return state;
     }
 };
