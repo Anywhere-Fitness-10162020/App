26dbcca14ae8a1c7753cd71f7fb2b07abc8a8756
kai-blt <blaitong@gmail.com>
2020-10-21 09:57:30 -0700

    Removed button from homepage, adjusted copy, added instructors :)

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,6 +8,7 @@ const HomepageContainer = styled.div`
     margin: auto;
     color: #fff;
 
+
     .hero {
         background: url('https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
         background-attachment: fixed;
@@ -39,6 +40,19 @@ const HomepageContainer = styled.div`
     }  
 `;
 
+const InstructorContainer = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    margin: 0 2%;
+
+    .instructor {
+        border-radius: 100%;
+        width: 70%;
+    }
+
+`;
+
 export default function Homepage(props) {
     return(
         <HomepageContainer>
@@ -55,22 +69,14 @@ export default function Homepage(props) {
             <div className="info">
                 <h2>Learn More</h2>
                 <p>
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
+                   Get the most out of your workouts with Anywhere Fitness! We provide
+                   professional guidance for all of your fitness needs. We offer a
+                   wide selection of classes from yoga to weightlifting and we'll work with
+                   you to get you in tip top shape within a matter of weeks. Each client will
+                   be paired with an instructor and a nutritionist to keep your workouts and 
+                   diet on track. Easy class scheduling for instructors and easy signup for clients.
+                   Manage all of your classes in one place.
                 </p>
-                <p>
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                </p>
-                <button>Learn More</button>
             </div>
             <div className="info2">
                 <h2>Classes </h2>
@@ -90,23 +96,29 @@ export default function Homepage(props) {
                 </p>
             </div>
             <div className="info3">
-                <h2>Experienced Instructors</h2>
-                <p>
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
-                </p>
-                <p>
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                    Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                    magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.
-                    Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor.
-                    Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.
-                </p>
+                <h2>Experienced Instructors</h2>     
+                <InstructorContainer>
+                    <div>
+                        <h3>Austin</h3>
+                        <img src='/assets/Austin.png' className="instructor" alt="Austin"></img>
+                    </div>
+                    <div>
+                        <h3>Eric</h3>
+                        <img src='/assets/Eric.png' className="instructor" alt="Eric"></img>
+                    </div>
+                    <div>
+                        <h3>Suzanne</h3>
+                        <img src='/assets/Suzanne.png' className="instructor" alt="Suzanne"></img>
+                    </div>
+                    <div>
+                        <h3>Brendan</h3>
+                        <img src='/assets/Brendan.png' className="instructor" alt="Brendan"></img>
+                    </div>
+                    <div>
+                        <h3>Sam</h3>
+                        <img src='/assets/Sam.png' className="instructor" alt="Sam"></img>
+                    </div>
+                </InstructorContainer>
             </div>
         </HomepageContainer>       
     )
