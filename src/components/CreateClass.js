abc1e8e17d187e48677361cfbe3c18874856a71b
kai-blt <blaitong@gmail.com>
2020-10-22 08:20:44 -0700

    Fixes to create class object

--- src/components/CreateClass.js
+++ src/components/CreateClass.js
@@ -141,14 +141,14 @@ export default function InstructorForm(props) {
 
         const newClass = {
             class_name: formValues.class_name,
-            class_duration: formValues.class_duration,
+            type: formValues.type,
+            start_time: `${formValues.class_date} ${formValues.start_time}`,
             class_intensity_level: formValues.class_intensity_level,
             class_city: formValues.class_city,
-            start_time: `${formValues.class_date} ${formValues.start_time}`,
-            class_timezone: formValues.class_timezone,
-            type: formValues.type,
+            attendee_count: 0,
             max_attendees: formValues.max_attendees,
-            instructor_id: formValues.id
+            class_duration: formValues.class_duration,
+            class_timezone: formValues.class_timezone
         }
 
         console.log(newClass)
